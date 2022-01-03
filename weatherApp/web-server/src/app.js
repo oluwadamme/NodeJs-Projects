805ea643f238dc09d2b379287307d224d26791ce
oluwadamme <adeniyidamilola246@gmail.com>
2022-01-03 15:28:26 +0100

    finished with web-server

--- weatherApp/web-server/src/app.js
+++ weatherApp/web-server/src/app.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs')
 
 const app = express()
 
 // Define paths for Express config
 const publicDirPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname,'../templates')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
 
 // Setup handlerbars engine and views location
 app.set('view engine', 'hbs') // it is used to set a value in a given express value
-app.set('views', viewsPath) //this is used to change the express lookup directory from views to another one
+app.set('views', viewsPath) // this is used to change the express lookup directory from views to another one
+hbs.registerPartials(partialsPath)
 
 // it is used to customise your server or setup static directory to serve
 app.use(express.static(publicDirPath))
@@ -40,6 +43,23 @@ app.get('/weather', (req, res)=>{
     res.send({
         forecast: 'it is 20 degrees',
         location: 'Ile Ife'
+    })
+})
+
+app.get('/help/*', (req, res)=>{
+    res.render('404',{
+        name:'Dammy',
+        title:'404',
+
+        error:'Help article not found'
+    })
+})
+
+app.get('*', (req, res)=>{
+    res.render('404',{
+        name:'Dammy',
+        title:'404',
+        error:'404 - Page not found'
     })
 })
 
