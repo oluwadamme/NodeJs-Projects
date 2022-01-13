f3b22c63c0aa42377b347b19afcf956c1b16783e
oluwadamme <adeniyidamilola246@gmail.com>
2022-01-14 00:59:56 +0100

    setup endpoints for users and tasks

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -2,34 +2,16 @@ const express = require('express');
 require('./db/mongoose') //this line ensures that our mongoose automatically connects to the db
 const User = require('./models/user')
 const Task = require('./models/task')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
 
-// resource creation
-// 'request, req' is data from the frontend user
-// 'response, res' is data from the backend
-app.post('/users',(req, res)=>{
-    const user = new User(req.body)
-
-    user.save().then((result) => {
-        res.status(201).send(result)
-    }).catch((error) => {
-       res.status(400).send(error.message)
-    });
-})
-
-app.post('/tasks',(request, response)=>{
-    const task = new Task(request.body)
-
-    task.save().then((result) => {
-        response.status(201).send(result)
-    }).catch((err) => {
-        response.status(400).send(err.message)
-    });
-})
 
 app.listen(port, ()=>{
     console.log('Server is up on port '+ port);
